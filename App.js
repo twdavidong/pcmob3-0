0bd8e411c19c3369b3641a79ff50f9956b12d73c
twdavidong <64457327+twdavidong@users.noreply.github.com>
2021-04-22 21:20:06 +0800

    First change

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const names = ["Alex", "Belinda","Charles","David"];
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<Text>{names[0]}</Text>
+<Text>{names[1]}</Text>
+<Text>{names[2]}</Text>
+<Text>{names[3]}</Text>
     </View>
   );
 }
